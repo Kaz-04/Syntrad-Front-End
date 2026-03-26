ffc5c0e8175e22c07100931c1fe69c0613352952
Kaz-04 <syntradltd@gmail.com>
2026-03-26 00:14:04 +0000

    Integrate Google Tag Manager scripts
    Added Google Tag Manager scripts for tracking.

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import { Outfit, Ovo} from "next/font/google";
 import "./globals.css";
 import ScrollToTop from "./components/ScrollTop";
@@ -28,6 +29,19 @@ export default function RootLayout({ children }) {
         <ScrollToTop />
         {children}
         <Footer />
+        <Script
+  src="https://www.googletagmanager.com/gtag/js?id=AW-17084572435"
+  strategy="afterInteractive"
+/>
+<Script id="google-ads-gtag" strategy="afterInteractive">
+  {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    window.gtag = gtag;
+    gtag('js', new Date());
+    gtag('config', 'AW-17084572435');
+  `}
+</Script>
       </body>
     </html>
   );
